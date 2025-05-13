5054bc87ee286cefad10f7c5cf43f10237e64185
Wilbert Oliver Chan Uc <131514400+wilbert2004@users.noreply.github.com>
2025-05-12 22:22:27 -0600

    navbar
    corregir ruta de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Inicio from "./pages/Inicio";
 import Nosotros from "./pages/Nosotros";
 import Ofrecemos from "./pages/Ofrecemos";
