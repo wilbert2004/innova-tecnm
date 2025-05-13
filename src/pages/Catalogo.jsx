de2ed7e7d450ae4800deb2219da01a28b9518b69
Wilbert Oliver Chan Uc <131514400+wilbert2004@users.noreply.github.com>
2025-05-12 22:45:45 -0600

    Update Catalogo.jsx

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import TituloCatalogo from "../components/componentsCatalogo/TituloCatalogo"; // Importa el componente TituloCatalogo
-import Semillas from "../components/componentsCatalogo/semillas"; // Importa el componente Semillas
+import Semillas from "../components/componentsCatalogo/semillas"; // Importa el componente Semillas\
+import TituloCatalogo from "../components/componentsCatalogo/titulocatalogo"; // Importa el componente TituloCatalogo
 const Catalogo = () => {
   return (
     <div>
-      {/* <div>renderizamos el componente de catalogotitulo</div> */}
+      {/* <div>renderizamos el componente de titulo</div> */}
       <TituloCatalogo />
       {/* <div>renderizamos el componente de semillas</div> */}
       <Semillas />
